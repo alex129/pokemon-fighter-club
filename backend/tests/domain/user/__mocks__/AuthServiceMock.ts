84ffc032f0e4ca3ae65ee9f7685f346f48e1fc7e
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-01 17:18:13 +0200

    refactor: :recycle: update login implementation in AuthServiceMock

--- backend/tests/domain/user/__mocks__/AuthServiceMock.ts
+++ backend/tests/domain/user/__mocks__/AuthServiceMock.ts
@@ -1,6 +1,11 @@
+import { Nullable } from '@/domain/shared/Nullable';
 import { AuthService } from '@/domain/user/contracts/AuthService';
 import { vi } from 'vitest';
 
 export default class AuthServiceMock implements AuthService {
-  login = vi.fn();
+  loginMock = vi.fn();
+
+  async login(email: string, password: string): Promise<Nullable<string>> {
+    return this.loginMock(email, password);
+  }
 }
